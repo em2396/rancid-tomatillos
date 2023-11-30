d4c4de5b9a7ed47750bc18c465e73768f3cb1c8e
Amy S <140124108+amysiu1028@users.noreply.github.com>
2023-11-30 15:25:34 -0500

    feat/add-comment

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import logo from './logo.svg';
 import movieData from './sampleData';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Test
+      
     </div>
   );
 }
 
 export default App;
+
+
+//QUESTION:
+//1. What's the purpose of getting sample data... rather than a GET request
