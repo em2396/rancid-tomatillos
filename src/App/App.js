59f0b1e95d0c4786ed065add6e8b1484f4dd2260
Amy S <140124108+amysiu1028@users.noreply.github.com>
2023-11-30 16:21:16 -0500

    feat/set state and add functional components

--- src/App/App.js
+++ src/App/App.js
@@ -1,22 +1,16 @@
-import { useState } from '.react';
+import { useState } from 'react';
 import movieData from '../sampleData';
-console.log("movieData", movieData)
 import Header from '../Header/Header'
 import Movies from '../Movies/Movies';
 import './App.css';
 
 function App() {
-  const movieData = movieData
-  const [ movies, setMovies ] = useState([movieData])
-
-  // function getMovies(movieData) {
-  //   setMovies(movieData.movies)
-  // }
+  const [ movies, setMovies ] = useState(movieData.movies)
 
   return (
     <div className="App">
         <Header />
-        <Movies movies={movies}/>
+        <Movies movies={movies} />
     </div>
   );
 }
