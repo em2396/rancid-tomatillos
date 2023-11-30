81d58dfbeb5c8b2d7c3af4ef8d8e465754b3e883
Amy S <140124108+amysiu1028@users.noreply.github.com>
2023-11-30 16:22:57 -0500

    feat/create individal movie details

--- src/Movie/Movie.js
+++ src/Movie/Movie.js
@@ -1,12 +1,11 @@
 import './Movie.css';
 
-export default function Movie({title, releaseDate, averageRating, posterImage, backDropPath,id}) {
+export default function Movie({title, releaseDate, averageRating, posterImage, backDropPath}) {
     return(
         <div>
             <h1>{title}</h1>
-            <img alt="poster image">{posterImage}</img>
-            <img alt="backdrop image"> {backDropPath}</img>
-            <p>This movie was released on {releaseDate} with an average rating of {averageRating}</p>
+            <img className="poster" src={posterImage} alt={`Poster for ${title}`}></img>
+            <p>This movie was released on {releaseDate} with an average rating of {averageRating}/10.</p>
             <button className="heart-button">Heart</button>
             <button className="five-star-rating">Five Star Rating Lives Here</button>
         </div>
