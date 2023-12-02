1a8b922672148b9c61005bfa3738c823826c4bf8
Emalee Poellot <emalee.poellot@gmail.com>
2023-12-02 13:39:04 -0700

    feat/Overlay text on image when hovering

--- src/Movie/Movie.js
+++ src/Movie/Movie.js
@@ -2,12 +2,15 @@ import './Movie.css';
 
 export default function Movie({title, releaseDate, averageRating, posterImage, displayMovie, id}) {
     return(
-        <div onClick={() => displayMovie(id)}>
+        <div className="poster-container" onClick={() => displayMovie(id)}>
             {/* <h1>{title}</h1> */}
             <img className="poster" src={posterImage} alt={`Poster for ${title}`}></img>
-            <p>This movie was released on {releaseDate} with an average rating of {averageRating}/10.</p>
-            <button className="heart-button">Heart</button>
-            <button className="five-star-rating">Five Star Rating Lives Here</button>
+            <p className="on-image">This movie was released on {releaseDate} with an average rating of {averageRating}/10.</p>
+            {/* <button className="on-image">Heart</button> */}
+            <img src="" className="heart-button"></img>
+            <button className="on-image">Five Star Rating Lives Here</button>
         </div>
     )
 }
+
+
