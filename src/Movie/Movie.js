3724200a6c013ae085681983f0e647afa494c46a
Emalee Poellot <emalee.poellot@gmail.com>
2023-12-01 18:09:13 -0700

    feat/Change height, width and text color

--- src/Movie/Movie.js
+++ src/Movie/Movie.js
@@ -3,7 +3,7 @@ import './Movie.css';
 export default function Movie({title, releaseDate, averageRating, posterImage, displayMovie, id}) {
     return(
         <div onClick={() => displayMovie(id)}>
-            <h1>{title}</h1>
+            {/* <h1>{title}</h1> */}
             <img className="poster" src={posterImage} alt={`Poster for ${title}`}></img>
             <p>This movie was released on {releaseDate} with an average rating of {averageRating}/10.</p>
             <button className="heart-button">Heart</button>
