abb47dd948fef56dac1e8b91a03c2a07800f1505
Amy S <140124108+amysiu1028@users.noreply.github.com>
2023-12-01 19:00:04 -0500

    feat/add movie details from data

--- src/MovieDetail/MovieDetail.js
+++ src/MovieDetail/MovieDetail.js
@@ -2,16 +2,16 @@
 
 
 export default function MovieDetail({selectedMovie, displayHomePage}) {
-    console.log(selectedMovie)
+    console.log("selectedMovie",selectedMovie)
     return (
         <div>
             <h1>{selectedMovie.title}</h1>
             <img className="singlePoster" src={selectedMovie.poster_path} alt={`Poster for ${selectedMovie.poster_path}`}></img>
             <img className="singlePoster" src={selectedMovie.backdrop_path} alt={`Poster for ${selectedMovie.backdrop_path}`}></img>
-            <p>Release date: 2021</p>
-            <p>overview: "Some overview that is full of buzzwords to attempt to entice you to watch this movie! Explosions! Drama! True love! Robots! A cute dog!"</p>
-            <p>average_rating: 6</p>
-            <p>genres: ["Drama"], budget:63000000, revenue:100853753, runtime:139, tagline: "It's a movie!"</p>
+            <p>Release date: {selectedMovie.release_date}</p>
+            <p>Overview: {selectedMovie.overiew}</p>
+            <p>Average rating: {(selectedMovie.average_rating).toFixed(2)}/10</p>
+            <p>Genre: {selectedMovie.genres}</p>
             <button onClick={(() => displayHomePage())}>Back To Home</button>
         </div>
     )
