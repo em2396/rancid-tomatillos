055f815995fda7941fcc229eb9487525002b6723
Amy S <140124108+amysiu1028@users.noreply.github.com>
2023-12-03 14:07:16 -0500

    feat/add sections and divs in moviedetails

--- src/MovieDetail/MovieDetail.js
+++ src/MovieDetail/MovieDetail.js
@@ -1,32 +1,43 @@
 import PropTypes from 'prop-types'
-
+import './MovieDetail.css'
 export default function MovieDetail({selectedMovie, selectedVideo, displayHomePage}) {
     //some movies have 2 videos, use conditional rendering
     // console.log("selectedMovie",selectedMovie)
     console.log("selectedVideo",selectedVideo)
+
+    const backgroundImageStyle = {
+        backgroundImage: `url(${selectedMovie.backdrop_path})`,
+        // backgroundSize: 'cover',
+        // backgroundPosition: 'center'
+    }
+
     return (
-        <div>
-            <h1>{selectedMovie.title}</h1>
-            <img className="singlePoster" src={selectedMovie.poster_path} alt={`Poster for ${selectedMovie.poster_path}`}></img>
-            <img className="singlePoster" src={selectedMovie.backdrop_path} alt={`Poster for ${selectedMovie.backdrop_path}`}></img>
-            <p>Release date: {selectedMovie.release_date}</p>
-            <p>Overview: {selectedMovie.overiew}</p>
-            <p>Average rating: {(selectedMovie.average_rating).toFixed(2)}/10</p>
-            <p>Genre: {selectedMovie.genres}</p>
-            <button onClick={(() => displayHomePage())}>Back To Home</button>
-            <h2>Videos</h2>
-            <div className="video-player">
-            <iframe
-              title="video player"
-              width="560"
-              height="315"
-              src={`https://www.youtube.com/embed/${selectedVideo.key}`}
-              frameBorder="0"
-              allowFullScreen
-            ></iframe>
-            </div>
-            
+        <>
+        <button onClick={(() => displayHomePage())}>Back To Home</button>
+        {/* <img className="backdrop-poster" src={selectedMovie.backdrop_path} alt={`Poster for ${selectedMovie.backdrop_path}`}></img> */}
+        <div className="selected-movie">
+            <section className='img-section' style={backgroundImageStyle}>
+                <div className='img-overlay'> 
+                    <img src={selectedMovie.poster_path} alt={`Poster for ${selectedMovie.poster_path}`}></img>
+                    <p>Release date: {selectedMovie.release_date}</p>
+                    <p>Overview: {selectedMovie.overiew}</p>
+                    <p>Average rating: {(selectedMovie.average_rating).toFixed(2)}/10</p>
+                    <p>Genre: {selectedMovie.genres}</p>
+                    <h2>Videos</h2>
+                    <div className="video-player">
+                        <iframe
+                        title="video player"
+                        width="560"
+                        height="315"
+                        src={`https://www.youtube.com/embed/${selectedVideo.key}`}
+                        frameBorder="0"
+                        allowFullScreen
+                        ></iframe>
+                    </div>
+                </div>
+            </section>
         </div>
+        </>
     )
 }
 
