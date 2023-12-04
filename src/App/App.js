df0e133a897ad3d7b315a4dcbaa94c1da8cdd454
Amy S <140124108+amysiu1028@users.noreply.github.com>
2023-12-03 21:15:18 -0500

    feat/add togglebottom and likedMovies

--- src/App/App.js
+++ src/App/App.js
@@ -12,23 +12,74 @@ function App() {
   const [ selectedVideo, setSelectedVideo ] = useState(null);
   const [ currentMovieIndex, setCurrentMovieIndex ] = useState(0);
   const [ error, setError ] = useState('')
-  const [ isLiked, setLiked ] = useState(false);
-  const likedMovies = []
+
+  // const [ isLiked, setLiked ] = useState(false);
+  // console.log("isLiked with object as initial state",isLiked)
+  const [ likedMovies, setLikedMovies ] = useState([]);
 
   function toggleLikeButton(id) {
     const likedMovie = movies.find(selectedMovie => {
       return selectedMovie.id === id;
     })
-    // console.log("likedMovie",likedMovie)
-    likedMovies.push(likedMovie)
-    if (likedMovies.includes(likedMovie)) {
-      setLiked(!isLiked)
-    }
-    isLiked ? setLiked(isLiked) : setLiked(!isLiked)
-    // setLiked(!isLiked)
-    // setLiked(likedMovie)
-    console.log("isLiked in App",isLiked)
+
+    const isMovieLiked = likedMovies.includes(likedMovie);
+
+    setLikedMovies(prevLikedMovies => {
+      if (!isMovieLiked) {
+        // setLikedMovies(prevLikedMovies => prevLikedMovies.filter(movie => movie.id !== likedMovie.id))
+        // If the movie is already liked, remove it from likedMovies
+        return [...prevLikedMovies, likedMovie];
+      } else {
+        // const updatedLikedMovies = likedMovies.filter((movie) => movie.id !== likedMovie.id)
+        //By using the callback version of setLikedMovies, you make sure that you are working with the latest state when updating it. The callback function receives the previous state as an argument (prevLikedMovies), and you can safely use it to update the state based on the previous state.
+        // setLikedMovies(updatedLikedMovies => [ ...updatedLikedMovies, likedMovie])
+         // If the movie is not liked, add it to likedMovies
+        return prevLikedMovies.filter(movie => movie.id !== likedMovie.id);
+      }
+    })
+    console.log("likedMovies",likedMovies)
   }
+
+  // const likedMovies = [ ]
+
+  // function toggleLikeButton(id) {
+    // const likedMovie = movies.find(selectedMovie => {
+    //   return selectedMovie.id === id;
+    // })
+  //   // console.log("likedMovie",likedMovie)
+  //   likedMovies.push(likedMovie)
+  //   if (likedMovies.includes(likedMovie)) {
+  //     setLiked(!isLiked)
+  //   }
+  //   isLiked ? setLiked(isLiked) : setLiked(!isLiked)
+  //   // setLiked(!isLiked)
+  //   // setLiked(likedMovie)
+  //   console.log("isLiked in App",isLiked)
+  // }
+
+  // function toggleLikeButton(id) {
+  //   const likedMovie = movies.find(selectedMovie => selectedMovie.id === id);
+
+  //   const updatedLikedMovies = [ ...likedMovie ]
+  //   console.log(updatedLikedMovies)
+  
+  //   // Check if the movie is already in likedMovies:
+  //   //The .some() method is a built-in JavaScript array method that checks whether at least one element in the array satisfies a given condition. It returns true if at least one element meets the condition, and false otherwise.
+  //   // const isMovieLiked = likedMovies.some(movie => movie.id === likedMovie.id);
+  //   const isMovieLiked = updatedLikedMovies.includes(likedMovie);
+  //   console.log("isMovieLiked",isMovieLiked)
+
+  //   if (isMovieLiked) {
+  //     // If the movie is already liked, remove it from likedMovies
+  //     const updatedLikedMovies = likedMovies.filter(movie => movie.id !== likedMovie.id);
+  //     setLikedMovies(updatedLikedMovies);
+  //   } else {
+  //     // If the movie is not liked, add it to likedMovies
+  //     setLikedMovies(prevLikedMovies => [...prevLikedMovies, likedMovie]);
+  //   }
+  // }  
+
+
 
   useEffect(() => {
     getMovies()
@@ -115,7 +166,7 @@ function App() {
       <>
       <button className="arrow left-arrow" onClick={arrowLeft}>&lt;</button>
       <button className="arrow right-arrow" onClick={arrowRight}>&gt;</button>
-      <Movies movies={movies} displayMovie={displayMovie} currentMovieIndex={currentMovieIndex} isLiked={isLiked} toggleLikeButton={toggleLikeButton}
+      <Movies movies={movies} displayMovie={displayMovie} currentMovieIndex={currentMovieIndex} likedMovies={likedMovies} toggleLikeButton={toggleLikeButton}
       />
       </>}
     </div>
