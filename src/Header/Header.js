835262a0c963ef36c26320bec302d1f1a3d88648
Emalee Poellot <emalee.poellot@gmail.com>
2023-12-06 18:19:33 -0700

    feat/Move className to img and not div

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,10 +1,10 @@
-
+import './Header.css'
 
 export default function Header() {
     return (
-        <div className="header">
+        <div>
             <a target="_top" href="https://flamingtext.com/">
-            <img
+            <img className="header"
             src="https://blog.flamingtext.com/blog/2023/11/30/flamingtext_com_1701310263_337249236.png"
             border="0"
             alt="rancid tomatillos"
